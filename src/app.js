187baa8df5a286d1574af12743de0446badcd52d
ramon oliveira <ramon30012000@gmail.com>
2022-01-28 18:15:10 -0300

    adds participants validation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 import express, { json } from "express";
 import cors from "cors";
+import joi from "joi";
 import mongoConnection from "./utils/mongoConnection.js";
 
 const app = express();
 app.use(json());
 app.use(cors());
 
+const participantSchema = joi.object({
+  name: joi.string().required().min(1),
+  laststatus: joi.number().required(),
+})
+
 app.post("/participants", async (req,res) => {
+  const validation = participantSchema.validate(req.body, {  abortEarly: false });
+
+  if (validation.error) {
+    const errorMessages = validation.error.details.map(({message}) => message);
+    res.status(422).send(`Validation error(s): ${errorMessages.join(", ")}`);
+    return;
+  }
+
   try {
     const mongoClient = await mongoConnection();
 
