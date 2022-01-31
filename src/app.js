19a8f8d88b1fe921d473f8f2f83cdd39537dc11b
ramon oliveira <ramon30012000@gmail.com>
2022-01-31 15:17:05 -0300

    feat: add the delete and put to the /messages/someId route and add the cleaner process of the user's name

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import dayjs from "dayjs";
 import mongoConnection from "./utils/mongoConnection.js";
 import { validateParticipant, validateMessage } from "./utils/validations.js";
 import validateDuplicity from "./utils/validateDuplicity.js";
+import { ObjectId } from "bson";
+import { stripHtml } from "string-strip-html";
 
 const app = express();
 app.use(json());
@@ -12,6 +14,8 @@ app.use(cors());
 
 
 app.post("/participants", async (req,res) => {
+  const nameCleaned = stripHtml(req.body.name).result.trim();
+
   const validation = validateParticipant(req.body, res);
   if (validation) return;
 
@@ -20,16 +24,16 @@ app.post("/participants", async (req,res) => {
 
     const participantsCollection = mongoClient.db("Bate-Papo_Uol").collection("participants");
 
-    const participantAlredyExist = await validateDuplicity(participantsCollection, { name: req.body.name }, res);
+    const participantAlredyExist = await validateDuplicity(participantsCollection, { name: nameCleaned }, res);
     if (participantAlredyExist) return;
 
 
-    await participantsCollection.insertOne({ ...req.body, lastStatus: Date.now() });
+    await participantsCollection.insertOne({ name: nameCleaned, lastStatus: Date.now() });
 
     const messagesCollection = mongoClient.db("Bate-Papo_Uol").collection("messages");
     await messagesCollection.insertOne(
       {
-        from: req.body.name, 
+        from: nameCleaned, 
         to: 'Todos', 
         text: 'entra na sala...', 
         type: 'status', 
@@ -63,9 +67,8 @@ app.get("/participants", async (req,res) => {
 
 app.post("/messages", async  (req, res) => {
   const message = { ...req.body, from: req.headers.user };
-  
+
   const validation = await validateMessage(message, res);
-  console.log(validation)
   if (validation) return;
 
   try {
@@ -76,6 +79,7 @@ app.post("/messages", async  (req, res) => {
     await messagesCollection.insertOne({ ...message, time: dayjs().locale("br").format('HH:mm:ss') });
 
     res.sendStatus(201);
+    mongoClient.close();
   } catch (error) {
     console.log(error)
     res.sendStatus(500);
@@ -97,10 +101,6 @@ app.get("/messages", async (req,res) => {
       { from: user }
     ]}).toArray();
 
-    messages.reverse();
-
-    console.log(messages.reverse().slice(-limit))
-
     res.status(200).send(messages.slice(-limit));
     mongoClient.close();
   } catch (error) {
@@ -109,14 +109,72 @@ app.get("/messages", async (req,res) => {
   }
 });
 
+app.delete("/messages/:messageId", async (req, res) => {
+  const user = req.headers.user;
+  const id = req.params.messageId;
+
+  try {
+    const mongoClient = await mongoConnection();
+    
+    const messagesCollection = mongoClient.db("Bate-Papo_Uol").collection("messages");
+
+    const message = await messagesCollection.findOne({ _id: new ObjectId(id) });
+
+    if (!message) {
+      res.sendStatus(404);
+      return;
+    } else if (message.from !== user) {
+      res.sendStatus(401);
+      return;
+    }
+    
+    await messagesCollection.deleteOne({ _id: new ObjectId(id) });
+    res.status(200).send("Deleted");
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.put("/messages/:messageId", async (req, res) => {
+  const id = req.params.messageId;
+  const newMessage = { ...req.body, from: req.headers.user };
+
+  const validation = await validateMessage(newMessage, res);
+  if (validation) return;
+
+  try {
+    const mongoClient = await mongoConnection();
+    
+    const messagesCollection = mongoClient.db("Bate-Papo_Uol").collection("messages");
+
+    const message = await messagesCollection.findOne({ _id: new ObjectId(id) });
+
+    if (!message) {
+      res.sendStatus(404);
+      return;
+    } else if (message.from !== req.headers.user ) {
+      res.sendStatus(401);
+      return;
+    }
+    
+    await messagesCollection.updateOne({ _id: new ObjectId(id) }, { $set: newMessage });
+    res.status(200).send("Updated");
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 app.post("/status", async (req, res) => {
+  const userCleaned = stripHtml(req.headers.user).result.trim();
+
   try {
     const mongoClient = await mongoConnection();
 
     const participantsCollection = mongoClient.db("Bate-Papo_Uol").collection("participants");
 
-    const updatement = await participantsCollection.updateOne({ name: req.headers.user }, { $set: { name: req.headers.user, lastStatus: Date.now() } });
+    const updatement = await participantsCollection.updateOne({ name: userCleaned }, { $set: { lastStatus: Date.now() } });
 
     if (updatement.modifiedCount === 0) {
       res.sendStatus(404);
@@ -130,5 +188,37 @@ app.post("/status", async (req, res) => {
     res.sendStatus(500);
   }
 });
+
+setInterval(async () => {
+  const now = Date.now();
+
+  try {
+    const mongoClient = await mongoConnection();
+
+    const participantsCollection = mongoClient.db("Bate-Papo_Uol").collection("participants");
+    
+    const offLineParticipants = await participantsCollection.find({ lastStatus: { $lt: now - 10000 }}).toArray();
+    await participantsCollection.deleteMany({ lastStatus: { $lt: now - 10000 }});
+
+    offLineParticipants.forEach( async ({name}) => {
+      const messagesCollection = mongoClient.db("Bate-Papo_Uol").collection("messages");
+      await messagesCollection.insertOne(
+        {
+          from: name, 
+          to: 'Todos', 
+          text: 'sai da sala...', 
+          type: 'status', 
+          time: dayjs().locale("br").format('HH:mm:ss'),
+        }
+      );
+      mongoClient.close();
+    })
+
+  } catch {
+    console.log(error);
+    res.sendStatus(500);
+  }
+
+}, 15000);
 
 app.listen(5000, console.log("Running in http://localhost:5000"))
